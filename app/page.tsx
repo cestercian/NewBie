4a2b0750eb3360147ec9715347aebc0d249d35b5
Cestercian <yashafaid@gmail.com>
2025-01-10 23:38:12 +0530

    V.1.3

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import FeaturedEvents from '../components/FeaturedEvents'
-import Services from '../components/Services'
-import Testimonials from '../components/Testimonials'
-import Footer from '../components/Footer'
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import FeaturedEvents from '@/components/FeaturedEvents'
+import Services from '@/components/Services'
+import Testimonials from '@/components/Testimonials'
+import ContactForm from '@/components/ContactForm'
+import Footer from '@/components/Footer'
 
 export default function Home() {
-  return (
-      <div className="min-h-screen bg-background">
-        <Header />
-        <main>
-          <Hero />
-          <FeaturedEvents />
-          <Services />
-          <Testimonials />
-        </main>
-        <Footer />
-      </div>
-  )
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <main>
+                <Hero />
+                <FeaturedEvents />
+                <Services />
+                <Testimonials />
+                <ContactForm />
+            </main>
+            <Footer />
+        </div>
+    )
 }
+
